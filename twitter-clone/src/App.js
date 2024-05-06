04ecc5dc682c7529b954aac11fc47b357f6f1797
Juliana Moreno <katherinmorca@unisabana.edu.co>
2024-05-05 21:52:42 -0500

    Add Config

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import './App.css';
+import Feed from './components/Feed';
+import Sidebar from './components/Sidebar';
 
 function App() {
-    return <div className="App">React App</div>;
+    return (
+        <div className="app">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+        </div>
+    );
 }
 
 export default App;
